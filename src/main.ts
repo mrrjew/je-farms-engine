d3e4152d00cecb712b4897e2288046c28acf9311
mrr.jew <100996700+mrrjew@users.noreply.github.com>
2024-07-25 16:13:50 +0000

    added cors

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,26 @@ import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  // configure dotenv
+  // Configure dotenv
   dotenv.config();
 
   const app = await NestFactory.create(AppModule);
+
+  // Configure CORS
+  app.enableCors({
+    origin: '*', // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow credentials if needed
+  });
+
+  // Configure global validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-    }),
+    })
   );
+
   await app.listen(3333);
 }
+
 bootstrap();
