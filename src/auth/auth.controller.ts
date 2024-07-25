f8ebe40e781c0e6544bd8858300b6ea634b0ced4
mrr.jew <100996700+mrrjew@users.noreply.github.com>
2024-07-25 14:49:40 +0000

    refactor(user): Added auth guard to profile

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,12 +2,14 @@ import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { Request } from 'express';
+import { JwtAuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Get('profile')
+  @UseGuard(JwtAuthGuard)
   profile(@Req() req: Request) {
     const user = req['user'];
     return user;
@@ -23,3 +25,7 @@ export class AuthController {
     return this.authService.signin(dto);
   }
 }
+function UseGuard(AuthGuard: any): (target: AuthController, propertyKey: "profile", descriptor: TypedPropertyDescriptor<(req: Request) => Express.User>) => void | TypedPropertyDescriptor<...> {
+  throw new Error('Function not implemented.');
+}
+
